82601f7d6a17da3e335bcbee6cdef3ec509c476a
elizabethkerubo02 <keruboelizabeth02@gmail.com>
2023-03-14 13:38:56 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+// const divide = (num1, num2) => {
+//     return (num1 + num2);
+// }
+
+// divide(2000,100);
+
+// has a function expression that divides 2000/1000
+const divide = function() {
+    return 2000 / 100;
+  }
+divide();
+
+// has an arrow function  that takes one parameter and multiplys it by itself
+const square = sq => sq * sq;
+
+const add = (num1,num2) => num1 + num2;
